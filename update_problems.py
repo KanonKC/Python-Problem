7501d129cfe2dcf2e817a5a966944cca11dd1ea1
KanonKC <kanonkce@gmail.com>
2022-06-19 00:27:26 +0700

    replace whitespace with underscore _

---
+++ update_problems.py
@@ -0,0 +1,33 @@
+import os
+import requests
+
+def pyfileFiler(files):
+    return [i for i in files if i[-3:] == ".py"]
+
+result = os.listdir('./')
+
+all_problems = []
+
+for i in result:
+    if i.split()[0] != "Week": continue
+    all_files = os.listdir(f'./{i}')
+    py_files = pyfileFiler(all_files)
+    link = [i.strip() for i in open(f'./{i}/problem.txt').readlines()]
+
+    week_prob = {}
+    for j in range(len(link)):
+        prob_name = py_files[j].split('-')[1][:-3]
+        week_prob[prob_name] = {
+            "number": j+1,
+            "name": prob_name,
+            "link": link[j],
+        }
+    
+    all_problems.append({
+        "week_no": int(i.split()[1]),
+        "title": i[10:],
+        "problems": week_prob
+    })
+
+requests.post('http://localhost:8000/mint-tutor/problems',json=all_problems)
+print("***UPDATE COMPLETED!***")
